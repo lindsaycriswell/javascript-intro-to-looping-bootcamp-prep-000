b00f9c90600baa57b3261a4963ab32dffb407dd9
Lindsay Criswell <lindsaycriswell@gmail.com>
2017-10-09 14:52:02 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,17 +8,12 @@ function forLoop(array){
   } return array;
 }
 
+function whileLoop(number){
+  while(number > 0){
+    console.log(number--);
+  }
+}
 
-// function forLoop(arr){
-//   for(var i = 0; i < 25; i++){
-//     if(i === 1){
-//       arr.push("I am 1 strange loop.");
-//     } else {
-//       arr.push("I am ${i} strange loops.");
-//     }
-//   } return arr;
-// }
-//
 // function whileLoop(num){
 //   while(num > 0){
 //     console.log(--num);
